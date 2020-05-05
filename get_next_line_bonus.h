5bdb51b8d46f0c2f2879cebc75f70f0ea28b6f06
Raphael GILLES <rgilles@student.42.fr>
2020-05-05 12:26:07 +0200

    added bonus & verified norm

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/02 15:42:00 by rgilles           #+#    #+#             */
+/*   Updated: 2020/05/02 15:42:01 by rgilles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(const char *s1, const char *s2);
+int		ft_strchr(const char *s, int c);
+char	*ft_substr(const char *str, unsigned int start, size_t len);
+
+#endif
